87ab02b33fcfc1fa919fcc2e2f8f0e9fe8ff4f64
Eric Nganga <ericmainaedits@gmail.com>
2021-09-19 01:35:20 -0500

    Add user serializer

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,11 @@
 class ReviewsController < ApplicationController
-    
+  def index
+    if params[:strain_id]
+      strain = Strain.find(params[:strain_id])
+      reviews = strain.reviews
+    else
+      reviews = Review.all
+    end
+    render json: reviews, include: :strain
+  end
 end
